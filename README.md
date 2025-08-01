eb91391f32f0c9325fc0e149e82600ef55c695c9
Qz <quadratz@proton.me>
2025-08-01 22:48:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# $package_name
+
+[![NPM Version](https://img.shields.io/npm/v/tgast-util-to-telegram-entity?style=for-the-badge&logo=npm&color=%23CB3837)](https://www.npmjs.com/package/$package_name)
+[![Static Badge](https://img.shields.io/badge/-Source_Code-%23181717?style=for-the-badge&logo=github)](https://github.com/tgast-ecosystem/$package_name)
+[![Static Badge](https://img.shields.io/badge/-%40tgastbot-%2326A5E4?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/tgastbot)
+
+## What is this?
+
+
+## When should I use this?
+
+
+## Install
+
+```sh
+# deno
+deno add npm:$package_name
+
+# bun
+bun add $package_name
+
+# pnpm
+pnpm add $package_name
+
+# yarn
+yarn add $package_name
+
+# npm
+npm install $package_name
+```
+
+## Use
+
+
+## API
+
---
+++ scripts/update-next-tag.ts
@@ -0,0 +1,50 @@
+/** Set the "next" tag to the newest version. */
+
+import type { SpawnSyncReturns } from 'node:child_process'
+import { execSync } from 'node:child_process'
+import { clean, gt } from 'semver'
+import pkgJson from '../package.json' with { type: 'json' }
+
+const pkgName: string = pkgJson.name
+let output: string
+
+try {
+  output = run(`pnpm view ${pkgName}@next version`)
+}
+catch (error: unknown) {
+  // Handle error when the next tag is not published yet.
+  if (error === null || typeof error !== 'object' || !('stderr' in error))
+    throw error
+
+  const { stderr } = error as SpawnSyncReturns<string>
+  const errMsg = 'No match found for version next'
+
+  if (stderr.includes(errMsg) === false)
+    throw error
+
+  // Set to the lowest possible semver to ensure the next tag points to the current version.
+  output = '0.0.0-alpha'
+}
+
+const nextVersion: string | null = clean(output)
+const currentVersion: string | null = clean(pkgJson.version)
+
+if (nextVersion === null) {
+  const msg = `'next' version '${output}' is invalid.`
+  throw new Error(msg)
+}
+
+if (currentVersion === null) {
+  const msg = `Current version '${pkgJson.version}' is invalid.`
+  throw new Error(msg)
+}
+
+const isCurrentVersionMostRecent: boolean = gt(currentVersion, nextVersion)
+
+if (isCurrentVersionMostRecent) {
+  run(`pnpm dist-tag add ${pkgName}@${currentVersion} next`)
+}
+
+function run(cmd: string): string {
+  return execSync(cmd, { encoding: 'utf8' })
+}
